4f74488bd06588ba232442a89a5afbc7fa85f530
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-05-08 16:49:44 +0200

    Used data source to zip functions file

---
+++ functions/health.mjs
@@ -0,0 +1,9 @@
+export const handler = async () => {
+
+    const response = {
+        status: '200',
+        body: 'OK',
+    };
+
+    return response;
+};
--- lambda.tf
+++ lambda.tf
@@ -3,7 +3,7 @@
 
 resource "aws_lambda_function" "health1" {
   function_name = "health-response"
-  filename      = "${path.module}/functions/health.zip"
+  filename      = "${path.module}/functions/health.mjs.zip"
   handler       = "health.handler"
   runtime       = "nodejs20.x"
   role          = aws_iam_role.lambda_exec.arn
@@ -18,12 +18,21 @@ resource "aws_lambda_permission" "health-invoke" {
   source_arn    = "${aws_apigatewayv2_api.terraform-aaignment-api.execution_arn}/*/*/health"
 }
 
+
+
+data "archive_file" "health" {
+  type             = "zip"
+  source_file      = "${path.module}/functions/health.mjs"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/functions/health.mjs.zip"
+}
+
 #  Quotes lambda
 
 resource "aws_lambda_function" "quotes" {
   function_name = "quetes"
   role          = aws_iam_role.lambda_exec.arn
-  filename      = "${path.module}/functions/quotes.zip"
+  filename      = "${path.module}/functions/quotes.mjs.zip"
   handler       = "quotes.handler"
   runtime       = "nodejs20.x"
 }
@@ -36,6 +45,12 @@ resource "aws_lambda_permission" "quotes-invoke" {
   source_arn    = "${aws_apigatewayv2_api.terraform-aaignment-api.execution_arn}/*/*/quotes"
 }
 
+data "archive_file" "quetes" {
+  type             = "zip"
+  source_file      = "${path.module}/functions/quotes.mjs"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/functions/quotes.mjs.zip"
+}
 
 
 resource "aws_iam_role" "lambda_exec" {
